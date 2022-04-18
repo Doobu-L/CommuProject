b2aa6785e81ee0ad476421e4e336884727d3091b
DESKTOP-SA0VI34\Seop <point455@bankq.co.kr>
2022-04-18 18:06:15 +0900

    init project

---
+++ community/src/main/java/com/ks/community/CommunityApplication.java
@@ -0,0 +1,24 @@
+package com.ks.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class CommunityApplication extends SpringBootServletInitializer {
+
+	static {
+		System.setProperty("spring.config.location", "classpath:/application.yml,classpath:/befit.yml");
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CommunityApplication.class);
+	}
+
+}
