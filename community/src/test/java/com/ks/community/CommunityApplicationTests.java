ec38d379a50ad4e6b142761ed8ac30f13a07165c
doobu-L <seop00513@gmail.com>
2022-04-18 23:21:29 +0900

    초기환경세팅 정리

--- community/src/test/java/com/ks/community/CommunityApplicationTests.java
+++ community/src/test/java/com/ks/community/CommunityApplicationTests.java
@@ -6,6 +6,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CommunityApplicationTests {
 
+	static {
+		System.setProperty("spring.config.location", "classpath:/application.yml,classpath:/befit.yml");
+	}
+
 	@Test
 	void contextLoads() {
 	}
