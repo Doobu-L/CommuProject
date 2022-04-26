620470be2bd418bb8c27cb5b1e7c92c8d1fb1144
doobu-L <seop00513@gmail.com>
2022-04-26 23:43:26 +0900

    User Controller create

---
+++ community/src/main/java/com/ks/community/controller/v1/UserController.java
@@ -0,0 +1,12 @@
+package com.ks.community.controller.v1;
+
+import lombok.NoArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@NoArgsConstructor
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+}
