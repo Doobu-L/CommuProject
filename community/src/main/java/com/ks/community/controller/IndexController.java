4ad0469b191056b7d125a15bd4b525ce0161eb42
doobu-L <seop00513@gmail.com>
2022-04-26 23:08:22 +0900

    Controller 패키지 생성 후 이동

--- community/src/main/java/com/ks/community/controller/IndexController.java
+++ community/src/main/java/com/ks/community/controller/IndexController.java
@@ -1,4 +1,4 @@
-package com.ks.community;
+package com.ks.community.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
