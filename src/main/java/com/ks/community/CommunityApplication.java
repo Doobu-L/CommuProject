4124402b9986f75b7673ab21088634b82e8e044f
doobu-L <seop00513@gmail.com>
2022-04-30 13:25:19 +0900

    gradle init

--- src/main/java/com/ks/community/CommunityApplication.java
+++ src/main/java/com/ks/community/CommunityApplication.java
@@ -7,18 +7,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 public class CommunityApplication extends SpringBootServletInitializer {
-
-	static {
-		System.setProperty("spring.config.location", "classpath:/application.yml,classpath:/commu.yml");
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(CommunityApplication.class, args);
 	}
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(CommunityApplication.class);
-	}
-
 }
