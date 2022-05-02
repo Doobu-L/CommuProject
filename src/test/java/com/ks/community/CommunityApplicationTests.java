7b97776ad82c462c745b57297936bbde91e6d44f
DESKTOP-SA0VI34\Seop <point455@bankq.co.kr>
2022-05-02 14:09:07 +0900

    코드정리

--- src/test/java/com/ks/community/CommunityApplicationTests.java
+++ src/test/java/com/ks/community/CommunityApplicationTests.java
@@ -6,10 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CommunityApplicationTests {
 
-	static {
-		System.setProperty("spring.config.location", "classpath:/application.yml,classpath:/commu.yml");
-	}
-
 	@Test
 	void contextLoads() {
 	}
