c9f155c9be7a27874bfa83bbc6447e270d69c205
DESKTOP-SA0VI34\Seop <point455@bankq.co.kr>
2022-05-03 10:49:07 +0900

    swagger Operation 추가

--- src/main/java/com/ks/community/controller/v1/FeedController.java
+++ src/main/java/com/ks/community/controller/v1/FeedController.java
@@ -2,6 +2,7 @@ package com.ks.community.controller.v1;
 
 import com.ks.community.dto.FeedDto;
 import com.ks.community.service.FeedService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -22,15 +23,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class FeedController {
   private final FeedService feedService;
 
+  @Operation(summary = "피드 추가", description = "")
   @PostMapping("/{userId}")
   public ResponseEntity newFeed(@PathVariable long userId,@RequestBody FeedDto req)
       throws Exception {
     return ResponseEntity.ok().body(feedService.newFeed(req,userId));
   }
 
+  @Operation(summary = "피드리스트 조회", description = "")
   @GetMapping("")
   public ResponseEntity getFeedList(@PageableDefault(sort = "createTimestamp",direction = Direction.DESC) Pageable pageable){
     return ResponseEntity.ok().body(feedService.getFeedList(pageable));
   }
+
+
 
 }
