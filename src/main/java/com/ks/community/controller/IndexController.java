f29aa7f28bbcbecaf93e148edfabc90451c190cc
DESKTOP-SA0VI34\Seop <point455@bankq.co.kr>
2022-05-06 18:05:15 +0900

    google OAuth login 테스트를 위한 html 추가

--- src/main/java/com/ks/community/controller/IndexController.java
+++ src/main/java/com/ks/community/controller/IndexController.java
@@ -1,15 +1,16 @@
 package com.ks.community.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("/index")
 public class IndexController {
 
-  @GetMapping("")
-  public String index(){
-    return "Hello Java";
+  @RequestMapping("")
+  public ModelAndView index(){
+    ModelAndView view = new ModelAndView("view/login.html");
+    return view;
   }
 }
